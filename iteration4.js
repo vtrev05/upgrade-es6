5ce6d6efc1720fb657fa81f741588cc85517fc67
vtrev05 <lamary@gmail.comvtrev05@gmail.com>
2021-10-07 15:52:41 +0200

    iteration4

---
+++ iteration4.js
@@ -0,0 +1,39 @@
+//4.1 Dado el siguiente array, devuelve un array con sus nombres utilizando .map().
+
+const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+const usersCopy = users.map(user => user.name)
+console.log(usersCopy) 
+
+//4.2 Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que empiece por 'A'.
+
+const users2 = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+const usersCopy2 = users.map((user) => {
+    return user.name.charAt(0) === 'A' ? user.name = 'Anacleto' : user.name;
+})
+console.log(usersCopy2) 
+
+//4.3 Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y añade al valor de .name el string ' (Visitado)' cuando el valor de la propiedad isVisited = true.
+
+const cities = [
+	{isVisited:true, name: 'Tokyo'}, 
+	{isVisited:false, name: 'Madagascar'},
+	{isVisited:true, name: 'Amsterdam'}, 
+	{isVisited:false, name: 'Seul'}
+];
+
+const citiesCopy = cities.map((city) => {
+    return city.isVisited === true ? city.name = `${city.name} Visitado` : city.name
+})
+console.log(citiesCopy)
